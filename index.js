b4d735502593ee3083aba7c52836ac3230777bbb
shuaibasghar <shuaibasghargtk@gmail.com>
2024-09-01 09:41:55 -0700

    path added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 const app = express();
 
 app.use(express.json());

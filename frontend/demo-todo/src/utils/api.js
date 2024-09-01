7f615407992c5a1f836bf6c09911f9db790b07f5
shuaibasghar <shuaibasghargtk@gmail.com>
2024-09-01 09:28:47 -0700

    fixed url

--- frontend/demo-todo/src/utils/api.js
+++ frontend/demo-todo/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "http://localhost:3000/",
+    // baseURL: "http://localhost:3000/",
+    baseURL: "http://35.154.46.198:3000",
 });
 
 export default api;
